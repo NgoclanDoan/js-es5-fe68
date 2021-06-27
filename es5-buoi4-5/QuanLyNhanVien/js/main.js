54c9030006332599cd60759b7ac51a917677d0e1
Lan Doan <ggdriveunlimited@mod.obagg.com>
2021-06-27 17:07:41 +0700

    add feature: Sửa nhân viên, cập nhật nhân viên, tìm kiếm nhân viên

--- es5-buoi4-5/QuanLyNhanVien/js/main.js
+++ es5-buoi4-5/QuanLyNhanVien/js/main.js
@@ -6,7 +6,7 @@ console.log(dsnv);
 var getEle = function (id) {
     return document.getElementById(id);
 };
-
+var validator = new Validator();
 var renderDSNV = function (dsnv) {
     var content = '';
     dsnv.forEach(function (nv, index) {
@@ -21,7 +21,7 @@ var renderDSNV = function (dsnv) {
             <td>${nv.ngaySinh}</td>
             <td>${nv.chucVu}</td>
             <td>
-            <button class="btn btn-success">Xem</button>
+            <button class="btn btn-success" data-toggle="modal" data-target="#myModal" onclick="suaNhanVien('${nv.maNV}')">Xem</button>
             <button class="btn btn-danger" onclick="xoaNhanVien('${nv.maNV}')">Xóa</button>
             </td>
         </tr>
@@ -34,12 +34,104 @@ function xoaNhanVien(maNV) {
     renderDSNV(dsnv.arr);
     setLocalStorage();
 }
+function suaNhanVien(maNV) {
+    getEle('btnThemNV').style.display = 'none';
+    getEle('btnCapNhat').style.display = 'block';
+
+    getEle('msnv').disabled = 'true';
+    // getEle('msnv').setAttribute('disabled', true);
+
+    var nhanVien = dsnv.layThongTinNhanVien(maNV);
+
+    /**
+     * Đổ data nhanVien lên modal
+     */
+    getEle('msnv').value = nhanVien.maNV;
+    getEle('name').value = nhanVien.tenNV;
+    getEle('email').value = nhanVien.email;
+    getEle('password').value = nhanVien.matKhau;
+    getEle('datepicker').value = nhanVien.ngaySinh;
+    getEle('chucvu').value = nhanVien.chucVu;
+}
+var validateInput = function (maNV, hoTen, email, matKhau) {
+    /**
+     * Kiểm tra dữ liệu
+     */
+    var isValid = true;
+    isValid &=
+        validator.kiemTraRong(
+            maNV,
+            'tbMaNV',
+            'Mã nhân viên không được để trống'
+        ) &&
+        validator.kiemTraDoDaiKyTu(
+            maNV,
+            'tbMaNV',
+            'Vui lòng nhập từ 4 - 10 ký tự!',
+            4,
+            10
+        );
+    isValid &=
+        validator.kiemTraRong(hoTen, 'tbTen', 'Họ tên không được để trống') &&
+        validator.kiemTraChuoi(hoTen, 'tbTen', 'Họ và tên không hợp lệ');
+
+    isValid &=
+        validator.kiemTraRong(email, 'tbEmail', 'Email không được để trống') &&
+        validator.kiemTraEmail(email, 'tbEmail', 'Email không đúng định dạng');
+
+    isValid &=
+        validator.kiemTraRong(
+            matKhau,
+            'tbMatKhau',
+            'Mật khẩu không được để trống'
+        ) &&
+        validator.kiemTraDoDaiKyTu(
+            maNV,
+            'tbMaNV',
+            'Vui lòng nhập từ 6 - 8 ký tự!',
+            6,
+            8
+        );
+    isValid &= validator.kiemTraChucVu(
+        'chucvu',
+        'tbChucVu',
+        'Bạn chưa chọn chức vụ'
+    );
+    if (!isValid) return;
+};
+/**
+ * Cập nhật nhân viên
+ */
+getEle('btnCapNhat').addEventListener('click', function () {
+    /**
+     * Lấy thông tin mới từ ô input
+     */
+    var maNV = getEle('msnv').value;
+    var hoTen = getEle('name').value;
+    var email = getEle('email').value;
+    var matKhau = getEle('password').value;
+    var ngaySinh = getEle('datepicker').value;
+    var chucVu = getEle('chucvu').value;
+    if (!validateInput(maNV, hoTen, email, matKhau)) return;
+    var nhanVien = new NhanVien(maNV, hoTen, email, matKhau, ngaySinh, chucVu);
+    dsnv.capNhatNhanVien(nhanVien);
+    getEle('btnDong').click();
+    renderDSNV(dsnv.arr);
+    setLocalStorage();
+});
+getEle('searchName').addEventListener('keyup', function () {
+    var keyword = getEle('searchName').value;
+    var mangTimKiem = dsnv.timKiemNhanVien(keyword);
+    renderDSNV(mangTimKiem);
+});
 //Lấy data từ local storage
 getLocalStorage();
 
 getEle('btnThem').addEventListener('click', function () {
     getEle('btnCapNhat').style.display = 'none';
+    getEle('btnThemNV').style.display = 'block';
 });
+
 getEle('btnThemNV').addEventListener('click', function () {
     var maNV = getEle('msnv').value;
     var hoTen = getEle('name').value;
@@ -52,7 +144,10 @@ getEle('btnThemNV').addEventListener('click', function () {
      * Khởi tạo đối tượng nhanVien từ lớp đối tượng nhanVien
      */
     var nhanVien = new NhanVien(maNV, hoTen, email, ngaySinh, chucVu);
-
+    /**
+     * Kiểm tra giá trị người dùng nhập vào
+     */
+    if (!validateInput(maNV, hoTen, email, matKhau)) return;
     /**
      * Thêm đối tượng nhanVien vào mảng
      */
@@ -69,6 +164,7 @@ getEle('btnThemNV').addEventListener('click', function () {
      */
     setLocalStorage();
 });
+
 function getLocalStorage() {
     /**
      * Lấy data từ local storage
--- es5-buoi4-5/QuanLyNhanVien/js/models/DanhSachNhanVien.js
+++ es5-buoi4-5/QuanLyNhanVien/js/models/DanhSachNhanVien.js
@@ -1,21 +1,53 @@
 function DanhSachNhanVien() {
     this.arr = [];
-    this.themNhanVien = function (nhanVien) {
-        this.arr.push(nhanVien);
-    };
-    this.timViTri = function (maNV) {
-        // findIndex
-        // var viTri = this.arr.findIndex(function(nv){
-        //     return maNV === nv.maNV;
-        // })
-        return this.arr.findIndex(function(nv){
-            return maNV === nv.maNV;
-        })
-    }
-    this.xoaNhanVien = function (maNV) {
-        var viTri = this.timViTri(maNV);
-        if (viTri !== -1) {
-            this.arr.splice(viTri, 1);
-        }
-    }
 }
+DanhSachNhanVien.prototype.themNhanVien = function (nhanVien) {
+    this.arr.push(nhanVien);
+};
+DanhSachNhanVien.prototype.timViTri = function (maNV) {
+    // findIndex
+    // var viTri = this.arr.findIndex(function(nv){
+    //     return maNV === nv.maNV;
+    // })
+    return this.arr.findIndex(function (nv) {
+        return maNV === nv.maNV;
+    });
+};
+DanhSachNhanVien.prototype.xoaNhanVien = function (maNV) {
+    var viTri = this.timViTri(maNV);
+    if (viTri !== -1) {
+        this.arr.splice(viTri, 1);
+    }
+};
+DanhSachNhanVien.prototype.suaNhanVien = function (maNV) {
+    var viTri = this.timViTri(maNV);
+    if (viTri !== -1) {
+        this.arr.splice(viTri, 1);
+    }
+};
+DanhSachNhanVien.prototype.layThongTinNhanVien = function (maNV) {
+    console.log('Prototype');
+    var viTri = this.timViTri(maNV);
+
+    // if (viTri !== -1) {
+    //     return this.arr[viTri];
+    // }
+    if (viTri !== -1) return this.arr[viTri];
+};
+console.log(DanhSachNhanVien.prototype);
+DanhSachNhanVien.prototype.capNhatNhanVien = function (nhanVien) {
+    var viTri = this.timViTri(nhanVien.maNV);
+
+    if (viTri !== -1) return (this.arr[viTri] = nhanVien);
+};
+DanhSachNhanVien.prototype.timKiemNhanVien = function (keyword) {
+    // var mangTimKiem = [];
+    // mangTimKiem = this.arr.filter(function (nv) {
+    //     return nv.hoTen.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+    // });
+    // return mangTimKiem;
+
+    return this.arr.filter(function (nv) {
+        return nv.hoTen.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+    });
+};
